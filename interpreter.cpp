cfb6208aa58ae730fc9e0bcfc9688f332c62467b
AgentRaider <KGex@outlook.com>
2017-06-22 10:36:30 +0800

    A new update in interpreter
    -Delete query without where clause can be handled now.

--- interpreter.cpp
+++ interpreter.cpp
@@ -4,6 +4,7 @@
 #include<cstring>
 #include<cstdio>
 #include<sstream>
+#include<ctime>
 #include "interpreter.h"
 #include "api.h"
 
@@ -38,17 +39,17 @@ void InterpretQuery(string query)
 			query.insert(i + 2, " ");
 			i += 2;
 		}
-		else if ((query[i] == '>' && query[i + 1] == '=') || (query[i] == '<' && query[i + 1] == '=') || (query[i] == '<' && query[i + 1] == '>'))//  >=  <= <>
-		{
-			query.insert(i, " ");
-			query.insert(i + 3, " ");
-			i += 3;
-		}
 		else if ((query[i] == '>' && query[i + 1] != '=') || (query[i] == '<' && query[i + 1] != '=' && query[i + 1] != '>'))//  >  <
 		{
 			query.insert(i, " ");
 			query.insert(i + 2, " ");
 			i += 2;
+		}
+		else if ((query[i] == '>' && query[i + 1] == '=') || (query[i] == '<' && query[i + 1] == '=') || (query[i] == '<' && query[i + 1] == '>'))//  >=  <= <>
+		{
+			query.insert(i, " ");
+			query.insert(i + 3, " ");
+			i += 3;
 		}
 		else if (query[i] == '=')
 		{
@@ -257,13 +258,18 @@ void ProcessDelete(stringstream& querystream)
 	string tablename;
 	string from;
 	string where;
-	querystream >> from >> tablename >> where;
-	if (from != "from" || where != "where")
+	querystream >> from >> tablename ;
+	if (from != "from")
 	{
 		cout << "Syntax Error!" << endl;
 		return;
 	}
 	Fitter fitter;
+	if (!(querystream >> where))// no where clause
+	{
+		Delete(tablename, fitter);
+		return;
+	}
 	string And;
 	do {
 		string itemname;
@@ -271,6 +277,7 @@ void ProcessDelete(stringstream& querystream)
 		string data;
 		querystream >> itemname >> oper >> data;
 		int op;
+		cout << oper << endl;
 		if (oper == "<") {
 			op = 0;
 		}
@@ -397,6 +404,9 @@ void ProcessExecfile(stringstream& querystream)
 	querystream >> filename;
 	cout << filename << endl;
 	FILE *fp = fopen(filename.c_str(), "r");
+	long start, stop;
+	start = clock();
+	//double CalculateTime(long start, long stop);
 	if (!fp)
 		printf("no such file\n");
 	else {
@@ -409,6 +419,8 @@ void ProcessExecfile(stringstream& querystream)
 			ExecfileLinetoQuery(line);
 		}
 	}
+	stop = clock();
+	cout << "Time cost: " << CalculateTime(start, stop) << "second" << endl;
 }
 void ExecfileLinetoQuery(string line)
 {
